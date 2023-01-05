cc703e5ddc5993be86053b2cacf3d138183fe12f
xoriston <1420damian@gmail.com>
2023-01-05 01:21:45 +0100

    refectored form interface

--- JS/script.js
+++ JS/script.js
@@ -5,13 +5,13 @@ import { v4 as uuidv4 } from 'https://cdn.skypack.dev/uuid';
 // prettier-ignore
 // const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-// const form = document.querySelector('.form');
-// const containerWorkouts = document.querySelector('.workouts');
-// const inputType = document.querySelector('.form__input--type');
-// const inputDistance = document.querySelector('.form__input--distance');
-// const inputDuration = document.querySelector('.form__input--duration');
-// const inputCadence = document.querySelector('.form__input--cadence');
-// const inputElevation = document.querySelector('.form__input--elevation');
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
 
 const geolocationSuccess = position => {
   const { latitude } = position.coords;
@@ -30,6 +30,13 @@ const geolocationSuccess = position => {
   
 
     map.on('click', (mapEvent) => {
+
+
+
+
+
+
+      
       console.log(mapEvent);
       const {lat, lng} = mapEvent.latlng
 
