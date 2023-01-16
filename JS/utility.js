261d661678a2ec15140a12788b625587c1440b3d
xoriston <1420damian@gmail.com>
2023-01-16 22:08:54 +0100

    added helper functions

---
+++ JS/utility.js
@@ -0,0 +1,13 @@
+const validInputs = (...inputs) => {
+  inputs.every(inp => {
+    Number.isFinite(inp);
+  });
+};
+
+const isPositiveNumber = (...inputs) => {
+  inputs.every(inp => {
+    inp > 0;
+  });
+};
+
+export { validInputs, isPositiveNumber };
